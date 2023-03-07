b0e3f4dc22ddb7713d70eccd24f5f7d5b26ab255
dariakrtn <dariakorotinawork@yandex.ru>
2023-03-07 13:42:02 +0300

    simple

---
+++ Assets/NameOfYourProject/Scripts/SampleScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Sample");
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Sample Unity Project
+
+Поменяйте на ссылку вашего проекта
+[Game Design Document](https://docs.google.com/document/d/1Cf15WGiFZbSyrMy5LaKT80Yt23PfXtv9-N1n2MBS6Bs/edit?usp=sharing)
